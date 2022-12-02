1cec81a907d148ea3f5a3cf8821437b7a0e7a22a
Stacey Kinder <76706319+StaceyKinder@users.noreply.github.com>
2022-12-02 05:09:51 +0200

    Update vars.go

--- vars.go
+++ vars.go
@@ -6,7 +6,7 @@ var (
 	flagServerName     = flag.String("name", "smtp2http", "the server name")
 	flagListenAddr     = flag.String("listen", ":smtp", "the smtp address to listen on")
 	flagWebhook        = flag.String("webhook", "http://localhost:8080/my/webhook", "the webhook to send the data to")
-	flagMaxMessageSize = flag.Int64("msglimit", 1024*1024*2, "maximum incoming message size")
+	flagMaxMessageSize = flag.Int64("msglimit", 1024*1024*1, "maximum incoming message size")
 	flagReadTimeout    = flag.Int("timeout.read", 5, "the read timeout in seconds")
 	flagWriteTimeout   = flag.Int("timeout.write", 5, "the write timeout in seconds")
 	flagAuthUSER       = flag.String("user", "", "user for smtp client")
